99d0c0ed67c4275d82ac799c9211e7e90b14a744
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2022-10-30 11:27:53 +0700

    thay đổi url

--- insertAWSDB_AuroraMySQL.py
+++ insertAWSDB_AuroraMySQL.py
@@ -3,7 +3,7 @@ import MySQLdb
 
 
 #Tạo kết nối với RDS Aurora MySQL và thêm dữ liệu vào
-conn = MySQLdb.connect(host='database-aurora.cluster-ctnk8ti4crcr.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
+conn = MySQLdb.connect(host='database-auroza.cluster-cnpnxtxbi6ci.us-east-1.rds.amazonaws.com', user='admin', passwd='lab-password', db='covid19_auroza', port=3306)
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
